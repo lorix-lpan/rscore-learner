6ff563c46d171b538d19d5a701fea74a7b132510
Lawrence Fei Pan <lawrence_pan@hot-shot.com>
2015-12-20 18:25:31 -0500

    finish calculator

--- app/components/calculator/calcController.js
+++ app/components/calculator/calcController.js
@@ -5,6 +5,24 @@ app.controller('calcController', ['$scope', function ($scope) {
     this.options = options;
   }
 
+  var rcalc = function (grade, stdev, avr, havr) {
+    if (grade > 100 || stdev > 100 || avr > 100 || havr > 100)
+      return "Error";
+
+    var zscore = (grade - avr) / stdev;
+    var gstr = (havr - 75) / 14;
+    var rscore = (zscore + gstr + 5) * 5; 
+    return !rscore ? "Error" : Math.floor(rscore*100)/100;
+  };
+
+  var getR = function () {
+    return rcalc($scope.grade.value, 
+                 $scope.stdev.value, 
+                 $scope.average.value, 
+                 $scope.haverage.value
+                );
+  };
+
   $scope.grade = new BoxInput(
     'Your Grade: ',
     75,
@@ -23,9 +41,27 @@ app.controller('calcController', ['$scope', function ($scope) {
     {floor: 0, ceil: 100}
   );
 
-  $scope.strength = new BoxInput(
-    'Group Strenth: ',
+  $scope.haverage = new BoxInput(
+    'High School Average: ',
     75,
     {floor: 0, ceil: 100}
   );
+
+  $scope.rscore = getR();
+
+  $scope.$watch('grade.value', function () {
+    $scope.rscore = getR();
+  });
+
+  $scope.$watch('stdev.value', function () {
+    $scope.rscore = getR();
+  });
+
+  $scope.$watch('average.value', function () {
+    $scope.rscore = getR();
+  });
+
+  $scope.$watch('haverage.value', function () {
+    $scope.rscore = getR();
+  });
 }]);
