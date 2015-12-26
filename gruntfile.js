59c542c3f7416ef09fc9c0e6d47fd64716365877
Lawrence Fei Pan <lawrence_pan@hot-shot.com>
2015-12-26 17:19:55 -0500

    add grunt and uglify js

---
+++ gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    uglify: {
+      dist: {
+        files: {
+          'dist/app.min.js': 
+            [
+              // libs
+              'assets/libs/angular/angular.js',
+              'assets/libs/angular-route/angular-route.js',
+              'assets/libs/angularjs-slider/dist/rzslider.js',
+              // angular files
+              'app/app.js', 
+              'app/services/languageService.js',
+              'app/components/calculator/calcController.js', 
+              'app/shared/animateTrans.js',
+              'app/shared/ch-lang/chLang.js',
+              'app/shared/text-title/textTitle.js',
+              'app/shared/main-input/boxSlider.js'
+            ]
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ["uglify"]);
+};
