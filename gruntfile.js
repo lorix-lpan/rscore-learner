c1b22b46ced9c939449326876c2bdd9e46a75fa6
Lawrence Fei Pan <lawrence_pan@hot-shot.com>
2016-01-01 19:41:51 -0500

    update index and grunt

--- gruntfile.js
+++ gruntfile.js
@@ -3,22 +3,32 @@ module.exports = function (grunt) {
     uglify: {
       dist: {
         files: {
-          'dist/app.min.js': 
+          'dist/app.min.js':
             [
               // libs
-              'assets/libs/angular/angular.js',
-              'assets/libs/angular-route/angular-route.js',
-              'assets/libs/angularjs-slider/dist/rzslider.js',
-              // angular files
-              'app/app.js', 
+              /* 'assets/libs/angular/angular.min.js',
+              'assets/libs/angular-aria/angular-aria.min.js',
+              'assets/libs/angular-messages/angular-messages.min.js',
+              'assets/libs/angular-animate/angular-animate.min.js',
+              'assets/libs/angular-material/angular-material.min.js',
+              'assets/libs/angular-route/angular-route.min.js',
+              */
+              'app/app.js',
               'app/config/route.js',
               'app/config/translate.js',
               'app/services/languageService.js',
-              'app/components/calculator/calcController.js', 
-              'app/shared/animateTrans.js',
-              'app/shared/ch-lang/chLang.js',
-              'app/shared/text-title/textTitle.js',
-              'app/shared/main-input/boxSlider.js'
+              'app/services/tabService.js',
+              'app/shared/translation/animateTrans.js',
+              'app/shared/translation/ch-lang/chLang.js',
+              'app/shared/navigation/text-title/textTitle.js',
+              'app/shared/navigation/nav-title/navTitle.js',
+              'app/shared/navigation/side-nav/sideNav.js',
+              'app/shared/navigation/nav-list/navList.js',
+              'app/shared/navigation/content-bar/contentBar.js',
+              'app/components/calculator/main-input/boxSlider.js',
+              'app/components/calculator/dynamics-tabs/dynamicsTabs.js',
+              'app/components/calculator/calc-rscore/calcRscore.js',
+              'app/components/calculator/calcController.js'
             ]
         }
       }
